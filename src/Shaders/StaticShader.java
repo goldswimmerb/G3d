208d275ae5f59e8e491b4a153dbfa0bf65a36534
goldswimmerb <goldswimmerbj@gmail.com>
2015-04-25 23:51:27 -0400

    Now has a maths class for transformation

--- src/Shaders/StaticShader.java
+++ src/Shaders/StaticShader.java
@@ -1,9 +1,11 @@
 package Shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram{
 private static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
 private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
-
+	private int location_transformationMatrix;
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
 		// TODO Auto-generated constructor stub
@@ -17,4 +19,12 @@ private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
 		
 	}
 
+	@Override
+	protected void getAllUniformLocations() {
+		// TODO Auto-generated method stub
+		location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+	}
+	public void loadTransformationMatrix(Matrix4f matrix){
+		super.loadMatrix(location_transformationMatrix, matrix);
+	}
 }
