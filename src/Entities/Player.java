24f8ef8f38fce13df1756ae6ab25e72ac4900646
goldswimmerb <goldswimmerbj@gmail.com>
2015-05-16 16:52:49 -0400

    Added jumping

--- src/Entities/Player.java
+++ src/Entities/Player.java
@@ -13,6 +13,11 @@ public class Player extends Entity {
 	private static final float TURN_SPEED = 160;
 	private float currentSpeed = 0;
 	private float currentTurnSpeed = 0;
+	private static final float GRAVITY = -50;
+	private static final float JUMP_POWER = 30;
+	private float upwardsSpeed = 0;
+	private static final float TERRAIN_HEIGHT = 0;
+	private boolean isInAir = false;
 	public Player(TexturedModel model, Vector3f position, float rotX,
 			float rotY, float rotZ, float scale) {
 		super(model, position, rotX, rotY, rotZ, scale);
@@ -26,10 +31,25 @@ public class Player extends Entity {
 			float dx = (float) (distance * Math.sin(Math.toRadians(super.getRotY())));
 			float dz = (float) (distance * Math.cos(Math.toRadians(super.getRotY())));
 			super.increasePosition(dx, 0, dz);
+			upwardsSpeed += GRAVITY * DisplayManager.getFrameTimeSeconds();
+			super.increasePosition(0, upwardsSpeed * DisplayManager.getFrameTimeSeconds(), 0);
+			if(super.getPosition().y<TERRAIN_HEIGHT){
+				upwardsSpeed = 0;
+				isInAir = false;
+				super.getPosition().y= TERRAIN_HEIGHT;
+			}
 			//System.out.println(currentSpeed);
 			//System.out.println(dx);
 			
 	}
+	private void jump(){
+		if(!isInAir){
+			this.upwardsSpeed = JUMP_POWER;
+			isInAir = true;
+		}
+		
+	}
+	
 	private void checkInputs(){
 	if(Keyboard.isKeyDown(Keyboard.KEY_W)){
 		this.currentSpeed = RUN_SPEED;
@@ -50,7 +70,9 @@ public class Player extends Entity {
 		this.currentTurnSpeed = 0;
 	}
 	
-	
+	if(Keyboard.isKeyDown(Keyboard.KEY_SPACE)){
+		jump();
+	}
 	}
 	
 }
